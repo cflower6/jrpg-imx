39fcf4b25fad1ac1171d8d2bd2fa851c1a87214c
chris <christopher.j.flowers6@gmail.com>
2024-02-06 12:44:17 -0500

    Reformatted BattleScreen creater

--- src/screens/battle/BattleScreen.ts
+++ src/screens/battle/BattleScreen.ts
@@ -14,17 +14,22 @@ export class BattleScreen {
         this.enemyContainer = new Container<DisplayObject>();
     }
 
+    /**This is here to build the battlescreen for player vs enemy*/
     async createBattleScreen() {
-        const resolvedTexture = Texture.from('/Users/chrisflowers/fleet/jrpg-imx/src/ui/BlankPanel-3.png');
+        /**Loads the png as a texture resource to be used later*/
+        const resolvedTexture = Texture.from('../src/ui/assets/BlankPanel-3.png');
+        /** We make the texture a sprite*/
         const playerScreen = Sprite.from(resolvedTexture);
-
+        /** Anchor the player screen in the middle*/
         playerScreen.anchor.set(0.5);
-
-        playerScreen.x = playerScreen.width / 2;
-        playerScreen.y = playerScreen.height / 2;
-
+        /** set's the screen in the middle of hte player container.*/
+        playerScreen.x = this.playerContainer.width / 2;
+        playerScreen.y = this.playerContainer.height / 2;
+        /**Adds the playerScreen to the player and enemy Container*/
+        /** Will pull this into two methods create playerscreen and enemyScreen*/
         this.playerContainer.addChild(playerScreen);
         this.enemyContainer.addChild(playerScreen);
+        /**Adds the screen to the main stage*/
         app.stage.addChild(this.playerContainer);
     }
 }
